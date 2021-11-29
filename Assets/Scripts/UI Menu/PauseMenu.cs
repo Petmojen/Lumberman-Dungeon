7766b8f3ce983b56bc802092ea3e75371b25615a
FlexLix1 <90191930+FlexLix1@users.noreply.github.com>
2021-11-29 14:58:51 +0100

    Flyttade saker runt

--- Assets/Scripts/UI Menu/PauseMenu.cs
+++ Assets/Scripts/UI Menu/PauseMenu.cs
@@ -5,16 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class PauseMenu : MonoBehaviour
 {
-    public static bool GameIsPaused = false;
-
+    public static bool gameIsPaused = false;
     public GameObject pauseMenuUI;
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
-            if (GameIsPaused)
+            if (gameIsPaused)
             {
                 Resume();
             }
@@ -29,21 +27,21 @@ public class PauseMenu : MonoBehaviour
     {
         pauseMenuUI.SetActive(false);
         Time.timeScale = 1f;
-        GameIsPaused = false;
+        gameIsPaused = false;
     }
 
     void Pause()
     {
         pauseMenuUI.SetActive(true);
         Time.timeScale = 0f;
-        GameIsPaused = true;
+        gameIsPaused = true;
     }
 
     public void LoadMenu()
     {
         SceneManager.LoadScene("MainMenu");
     }
-    
+
     public void QuitGame()
     {
         Application.Quit();

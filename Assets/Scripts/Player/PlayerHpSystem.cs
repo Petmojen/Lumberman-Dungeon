481d8489d1379d9274869c642e8ac58b699a6ebc
LQMemer <42935807+MemerRedeemer@users.noreply.github.com>
2021-11-30 11:24:04 +0100

    Fixed hp

--- Assets/Scripts/Player/PlayerHpSystem.cs
+++ Assets/Scripts/Player/PlayerHpSystem.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
-public class PlayerHpSystem : MonoBehaviour
+public class PlayerHpSystem:MonoBehaviour
 {
     [SerializeField] Slider sliderHealth;
+    bool invicible = false;
     float health = 100;
 
     [SerializeField] GameObject[] armorSprite;
@@ -14,6 +15,7 @@ public class PlayerHpSystem : MonoBehaviour
 
     public void TakeDamage(float damage)
     {
+        invicible = true;
         if(armor > 0)
         {
             armor--;
@@ -22,36 +24,44 @@ public class PlayerHpSystem : MonoBehaviour
             health -= damage;
             sliderHealth.value = health / 100;
         }
+        Invoke(nameof(Vinsible), 2f);
     }
 
     public void UpdateArmor()
     {
         for(int i = 0; i < armorSprite.Length; i++)
         {
-            if (i < armor)
+            if(i < armor)
             {
                 armorSprite[i].SetActive(true);
-            }
-            else
-            {
+            } else {
                 armorSprite[i].SetActive(false);
             }
         }
     }
 
+    void Vinsible()
+    {
+        invicible = false;
+        CancelInvoke();
+    }
+
     void OnTriggerEnter2D(Collider2D collision)
     {
-        switch(collision.gameObject.tag)
+        if(!invicible)
         {
-            case "MinionShot":
+            switch(collision.gameObject.tag)
+            {
+                case "MinionShot":
                 TakeDamage(10);
                 break;
-            case "Boss":
+                case "Boss":
                 TakeDamage(25);
                 break;
-            case "Leaf":
+                case "Leaf":
                 TakeDamage(10);
                 break;
+            }
         }
     }
 }

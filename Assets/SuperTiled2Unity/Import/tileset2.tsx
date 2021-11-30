d7095350e5d23e3a1217a899a1ad7fbb8f841ecb
JacksterC <90191843+JacksterC@users.noreply.github.com>
2021-11-30 10:39:09 +0100

    added testscene for pathfinding test

---
+++ Assets/SuperTiled2Unity/Import/tileset2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="tileset2" tilewidth="16" tileheight="16" tilecount="128" columns="16">
+ <image source="../../../../../GitHub/Lumberman-Dungeon/Assets/SuperTiled2Unity/Import/Atlas.png" width="256" height="128"/>
+</tileset>

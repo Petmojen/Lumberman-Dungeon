c0004f2cb6589e6294b0ef72bd6572799d96c92e
JacksterC <90191843+JacksterC@users.noreply.github.com>
2021-12-13 19:21:00 +0100

    map update

---
+++ Assets/SuperTiled2Unity/Import/propsatlas.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="propsatlas" tilewidth="16" tileheight="16" tilecount="405" columns="27">
+ <image source="propsatlas.png" width="435" height="255"/>
+</tileset>

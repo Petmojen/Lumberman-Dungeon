6b806b529a618e90c160b53554cb2d974010ff01
JacksterC <90191843+JacksterC@users.noreply.github.com>
2021-12-21 00:16:36 +0100

    Tutorial updates

--- Assets/Scripts/Lights/HighlightItem.cs
+++ Assets/Scripts/Lights/HighlightItem.cs
@@ -5,16 +5,19 @@ using UnityEngine.Experimental.Rendering.Universal;
 
 public class HighlightItem : MonoBehaviour
 {
+	TutorialTextScript textScript;
 	Light2D lightSource;
 	
     void Start()
     {
 		lightSource = gameObject.GetComponentInChildren<Light2D>();
+		textScript = GameObject.FindObjectOfType(typeof(TutorialTextScript)) as TutorialTextScript;
     }
 
 	void OnTriggerEnter2D(Collider2D collision)
 	{
 		lightSource.intensity = 2f;
+		textScript.typeOfItem = gameObject.name;
     }
     
 

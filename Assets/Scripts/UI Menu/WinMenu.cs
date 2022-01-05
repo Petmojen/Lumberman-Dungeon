795209daca63379ac2e4e676e1b0afd8d43935f0
JacksterC <90191843+JacksterC@users.noreply.github.com>
2022-01-05 02:31:08 +0100

    last for tonight

--- Assets/Scripts/UI Menu/WinMenu.cs
+++ Assets/Scripts/UI Menu/WinMenu.cs
@@ -5,18 +5,45 @@ using UnityEngine;
 public class WinMenu : MonoBehaviour
 {
     [SerializeField] GameObject WinText;
+	public GameObject FadeToBlackForMenuUI;
     BossHP bossHPScript;
+	FadeToBlack fadeToBlack;
+	FadeToBlackForMenu fadeToBlackForMenu;
 
     void Start()
     {
-        bossHPScript = GameObject.FindGameObjectWithTag("Boss").GetComponent<BossHP>(); 
+        bossHPScript = GameObject.FindGameObjectWithTag("Boss").GetComponent<BossHP>();
+		fadeToBlack = GameObject.FindObjectOfType(typeof(FadeToBlack)) as FadeToBlack;
+		fadeToBlackForMenu = GameObject.FindObjectOfType(typeof(FadeToBlackForMenu)) as FadeToBlackForMenu;			
     }
 
     void Update()
     {
-        if(bossHPScript.bossDead) {
-            WinText.SetActive(true);
-            Time.timeScale = 0f;
+        if(bossHPScript.bossDead)
+		{
+			Invoke(nameof(FadeOut), 3f);
+			Invoke(nameof(FadeIn), 6f);
+			DifficultyManager.difficultyLevel++;
+			
         }        
     }
+
+	void FadeIn()
+	{
+		if (FadeToBlackForMenuUI.activeSelf)
+		{
+			fadeToBlackForMenu.Fade(true);
+			
+			if (fadeToBlackForMenu.image.color.a <= 0f)
+			{
+				WinText.SetActive(true);
+				FadeToBlackForMenuUI.SetActive(false);
+			}
+		}
+	}
+	
+	void FadeOut()
+	{
+		fadeToBlack.Fade(false);
+	}
 }
